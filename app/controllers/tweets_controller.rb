87cec26a7928cb5b1107b12d8701fd2701d69b7b
notorious94 <akib94dhk@gmail.com>
2022-05-19 03:17:31 +0600

    alert message added

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -16,17 +16,24 @@ class TweetsController < ApplicationController
   def create
     @tweet = current_user.tweets.new(tweet_params)
     if @tweet.save
+      flash[:success] = 'Tweet posted successfully'
       redirect_to tweets_path
     else
-      flash[:alert] = 'Please review the following problems'
-      render :edit
+      @tweets = Tweet.all.order(id: :desc)
+      flash[:alert] = 'Operation could not be completed.'
+      render :index
     end
   end
 
   def edit; end
 
   def update
-    redirect_to tweet_path(@tweet) if @tweet.update(tweet_params)
+    if @tweet.update(tweet_params)
+      redirect_to tweet_path(@tweet)
+    else
+      flash[:alert] = 'Please review the following problems'
+      render :edit
+    end
   end
 
   def show; end
